43ee322ee7920127b475420a63e69c2b7642faf4
mubolin2001 <91496644+mubolin2001@users.noreply.github.com>
2024-10-19 20:04:17 +0000

    added data.py and frame.py

--- data.py
+++ data.py
@@ -1,7 +1,14 @@
 import json
-def load_data(filename):
+from tweet import Tweet
+def load_data(filename, load_length = None):
+    
     with open(filename, 'r') as file:
         data = json.load(file)
-    return data
+    tweets = []
+    for tweet in data:
+        new_tweet  = Tweet( id = tweet['id'], text = tweet['text'], user = tweet['user'],
+        timestamp = tweet['timestamp_ms'])
+        tweets.append(tweet)
+    return tweets
 data = load_data("gg2013.json")
-print(data[0])
+print(type(data[0]))
